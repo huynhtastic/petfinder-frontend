9b9473ca113de2379545f02ab7f15350cc4ac593
Richard L Huynh <huynhtastic@gmail.com>
2019-02-25 10:02:59 -0600

    now receiving responses from backend

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,38 @@
+import fetch from 'node-fetch';
 import React, { Component } from 'react';
 import './Home.css';
+import env from '../../env.js';
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: '',
+    }
+  }
+
+  componentDidMount() {
+    fetch(`${env.apiUrl}/`)
+      .then((res) => {
+        if (res.status === 200) {
+          console.log(res);
+          return res.json();
+        }
+      })
+      .then((json) => {
+        console.log(json);
+        //this.setState({message: json.message});
+      })
+  }
+
   render() {
     return (
       <div className='Home'>
         <div className='Lander'>
           <h1>Petfinder Search</h1>
           <p>A website to find your perfect pet!</p>
+          <p>{this.state.message}</p>
         </div>
       </div>
     );
---
+++ src/env.js
@@ -0,0 +1,3 @@
+export default {
+  apiUrl: 'http://localhost:3000' || process.env.NODEJS_ENV,
+};
